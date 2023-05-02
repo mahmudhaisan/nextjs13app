3247124e7810b25d69c5335a646a182428f8ae3a
mahmudhaisan <mahmudhaisan@gmail.com>
2023-05-02 06:13:29 +0600

    setup next13 app

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,11 +1,11 @@
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
    <>
 
    <div className='bg-black text-white'>
-Hello From Homepage
+            Hello From Products
 
    </div>
    </>
